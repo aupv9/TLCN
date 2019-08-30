241fe8856eb76b67f25c8b64f9705ba5116a8dff
au.phan <aupv96@gmail.com>
2019-08-30 09:55:04 +0700

    30/8/2019

--- src/components/search-box-list/index.js
+++ src/components/search-box-list/index.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
-
+import './style.scss'
 class SearchBox extends Component {
     render() {
         return (
             <>
-
+                <div id="search-box-list">
+                        11
+                </div>
             </>
         );
     }
