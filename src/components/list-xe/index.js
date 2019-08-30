79a59558d9bc723796125f1e9d234eb101e89b39
au.phan <aupv96@gmail.com>
2019-08-30 09:04:49 +0700

    cm2

--- src/components/list-xe/index.js
+++ src/components/list-xe/index.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import './style.scss'
 import {Link} from "react-router-dom";
+import IntroList from "../intro-list";
+import SearchBox from "../search-box-list";
 
 class ListXe extends Component {
     componentWillMount() {
@@ -10,31 +11,8 @@ class ListXe extends Component {
     render() {
         return (
             <>
-                <div id="list-main">
-                    <div id="intro-list">
-                        <div className="container">
-                            <div className="col-md-12 col-sm-12 col-xs-12 ">
-                                <div className="row">
-                                    <div className="col-md-6 hidden-sm hidden-xs">
-                                        <h1 className="title-intro">
-                                            <span className="hidden-sm hidden-xs">Vé xe từ</span>
-                                        </h1>
-                                    </div>
-                                    <div className="col-md-6 hidden-sm hidden-xs">
-                                        <h6 className="mt0 route-title hidden-sm hidden-xs route-title-bus-ticket"  style={{float:'right'}}>
-                                            <span className="mt0 mb0 text-gray">
-                                                <Link to={"/"}>Vé Xe Khách</Link>
-                                                >
-                                                <Link to={"/"}>Xe đi </Link>
-                                            </span>
-                                        </h6>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
+               <IntroList/>
+                <SearchBox/>
             </>
         );
     }
