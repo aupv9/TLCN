1c359d95fa8e9a62fa7e5d6572f86b8185b19f70
au.phan <aupv96@gmail.com>
2019-09-02 23:23:50 +0700

    2/9/2019

--- src/components/home-main/index.js
+++ src/components/home-main/index.js
@@ -1,7 +1,18 @@
 import React, {Component} from 'react';
 import './style.scss'
+import DatePicker from "react-datepicker";
 
+import "react-datepicker/dist/react-datepicker.css";
 class Home_Main extends Component {
+    state = {
+        startDate: new Date()
+    };
+
+    handleChange = date => {
+        this.setState({
+            startDate: date
+        });
+    };
     render() {
         return (
             <>
@@ -19,6 +30,33 @@ class Home_Main extends Component {
                                                         <h2 className="subTitle">Search and compare hundreds of travel
                                                             sites at once</h2>
                                                     </div>
+                                                   <div className="form-section">
+                                                        <div className="form-container">
+                                                            <div className="base-form-search">
+                                                                 <div className={"form-header"}>
+                                                                     <div className="displayBlock">
+                                                                         <button className="button-radio active-button-radio">Khứ hồi</button>
+                                                                         <button  className="button-radio">Một chiều</button>
+                                                                     </div>
+                                                                 </div>
+                                                                <div className="form-search-wrapper">
+                                                                    <div className="keel-grid">
+                                                                        <div className="col-fields">
+                                                                            <input type="text"  className="col-field" placeholder="Từ đâu?"/>
+                                                                            <button id="switch-btn" className=""><i
+                                                                                className="fas fa-exchange-alt"></i></button>
+                                                                            <input type="text"  className="col-field"  placeholder="Đến đâu?"/>
+                                                                             <DatePicker className={"col-field"}
+                                                                                selected={this.state.startDate}
+                                                                                onChange={this.handleChange}
+                                                                                />
+                                                                             <button className="button-submit">Tìm Kiếm</button>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                   </div>
                                                </section>
                                             </div>
                                         </div>
@@ -28,6 +66,14 @@ class Home_Main extends Component {
                         </div>
                     </div>
                 </main>
+                <div className="smartbox smartbox-air">
+                    <ul>
+                        <li>1</li>
+                        <li>2</li>
+                        <li>3</li>
+
+                    </ul>
+                </div>
             </>
         );
     }
