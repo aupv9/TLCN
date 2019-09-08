5fb2ccbfffb47cc3af8fa251c7a09f51df76bea6
au.phan <aupv96@gmail.com>
2019-09-08 11:21:34 +0700

    deploy update

--- src/components/home-main/index.js
+++ src/components/home-main/index.js
@@ -3,7 +3,7 @@ import './style.scss';
 import * as LIST from '../../contants';
 import "react-datepicker/dist/react-datepicker.css";
 import * as _ from "lodash";
-import FormSearchMain from "../form-search-main";
+import DatePicker from "react-datepicker";
 
 class Home_Main extends Component {
     //* khởi tạo các biến local để xử lý
@@ -143,7 +143,47 @@ class Home_Main extends Component {
                                                         <h2 className="subTitle">Search and compare hundreds of travel
                                                             sites at once</h2>
                                                     </div>
-                                                <FormSearchMain />
+                                                   <div className="form-section">
+                                                       <div className="form-container">
+                                                           <div className="base-form-search">
+                                                               <div className={"form-header"}>
+                                                                   <div className="displayBlock">
+                                                                       <button className="button-radio active-button-radio">Khứ hồi</button>
+                                                                       <button  className="button-radio">Một chiều</button>
+                                                                   </div>
+                                                               </div>
+                                                               <div className="form-search-wrapper">
+                                                                   <div className="keel-grid">
+                                                                       <div className="col-fields">
+                                                                           <input onKeyDown={(e)=>this.onSearch(1,e)}
+                                                                                  type="text"
+                                                                                  onChange={(e)=>this.onSearch(1,e)}
+                                                                                  onBlur={this.offList}
+                                                                                  value={this.state.nameStart}
+                                                                                  className="col-field"
+                                                                                  placeholder="Từ đâu?"/>
+                                                                           <button id="switch-btn"
+                                                                                   className=""
+                                                                                   onClick={this.onSwitch} >
+                                                                               <i className="fas fa-exchange-alt"/>
+                                                                           </button>
+                                                                           <input type="text"
+                                                                                  onChange={(e)=>this.onSearch(2,e)}
+                                                                                  className="col-field"
+                                                                                  placeholder="Đến đâu?"
+                                                                                  onBlur={this.offList}
+                                                                                  value={this.state.nameEnd}/>
+                                                                           <DatePicker className={"col-field"}
+                                                                                       selected={this.state.startDate}
+                                                                                       onChange={this.handleChange}
+                                                                           />
+                                                                           <button className="button-submit">Tìm Kiếm</button>
+                                                                       </div>
+                                                                   </div>
+                                                               </div>
+                                                           </div>
+                                                       </div>
+                                                   </div>
                                                </section>
                                             </div>
                                         </div>
