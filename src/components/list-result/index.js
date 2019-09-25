c44e22e04b87650c582ad4df02244ff3f7c9eba4
AuPhan <aupv96@gmail.com>
2019-09-25 23:51:52 +0700

    update front 25/9

--- src/components/list-result/index.js
+++ src/components/list-result/index.js
@@ -6,11 +6,28 @@ class ListResult extends Component {
             <>
                 <div className="resultsContainer">
                     <div className="search-result-list">
+                        {/*Begin*/}
                         <div className="cars-result">
                             <div className="result-wrapper">
                                 <div className="result-inner">
                                     <div className="grid-inner">
-
+                                        <div className="col-info">
+                                            <div className="main-info">
+                                                <div className="cars-result-info">
+                                                    <ol className="cars">
+                                                        <li className="car">
+                                                            <div className="cars-result-lefinfo">
+                                                                <div className="v-c-p">
+                                                                    <div className="col-field carrier">
+                                                                            11
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </li>
+                                                    </ol>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
