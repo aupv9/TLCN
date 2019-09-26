54120ed6fc24b5a5f30d19ea4487af6280b54c6c
AuPhan <aupv96@gmail.com>
2019-09-27 00:20:16 +0700

    update front 27/9

--- src/components/list-result/index.js
+++ src/components/list-result/index.js
@@ -19,7 +19,26 @@ class ListResult extends Component {
                                                             <div className="cars-result-lefinfo">
                                                                 <div className="v-c-p">
                                                                     <div className="col-field carrier">
-                                                                            11
+                                                                        <div className="top">
+                                                                            <img src={"https://content.r9cdn.net/rimg/provider-logos/airlines/v/VJ.png?crop=false&amp;width=108&amp;height=92&amp;fallback=default2.png&amp;_v=b07fe173a0b8c5b03cf17378f234f88817558185"}
+                                                                                 alt="logo VietJet Air" />
+                                                                        </div>
+                                                                        <div className="bottom">
+                                                                            111
+                                                                        </div>
+
+                                                                    </div>
+                                                                    <div className="col-field time-start">
+                                                                            222
+                                                                    </div>
+                                                                    <div className="col-field route">
+                                                                        <div className="axis"></div>
+                                                                    </div>
+                                                                    <div className="col-field time-end">
+                                                                        222
+                                                                    </div>
+                                                                    <div className="col-field info-car">
+                                                                        ss
                                                                     </div>
                                                                 </div>
                                                             </div>
